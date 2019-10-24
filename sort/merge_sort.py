2ceee59b71438b1817c63dd9bb60ea64efcbfc56
syd-a <sahmed.send@gmail.com>
2019-10-24 13:33:00 -0400

    Reduce space complexity of merge sort implementation

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -10,20 +10,23 @@ def merge_sort(arr):
 
 def merge(sorted_left, sorted_right):
     result = []
-    while(len(sorted_left) > 0 and len(sorted_right) > 0):
-        head_left = sorted_left[0]
-        head_right = sorted_right[0]
-        if head_left <= head_right:
-            result.append(head_left)
-            sorted_left = sorted_left[1:]
+    i = 0
+    j = 0
+    while (i < len(sorted_left)) and (j < len(sorted_right)):
+        if sorted_left[i] <= sorted_right[j]:
+            result.append(sorted_left[i])
+            i += 1
         else:
-            result.append(head_right)
-            sorted_right = sorted_right[1:]
-    result.extend(sorted_left)
-    result.extend(sorted_right)
+            result.append(sorted_right[j])
+            j += 1
+    result.extend(sorted_left[i:])
+    result.extend(sorted_right[j:])
     return result
 
-if __name__ == '__main__':
+def use_merge_sort():
     arr = [7, 5, 17, 9, 31, 22]
-    print(arr)
-    print(merge_sort(arr))
+    print("unsorted array: ", arr)
+    print("sorted array: ", merge_sort(arr))
+
+if __name__ == '__main__':
+    use_merge_sort()
