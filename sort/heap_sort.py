cd537982abc75d82c6bd957b4f4e1a262ec91a95
syd-a <sahmed.send@gmail.com>
2019-10-28 18:27:56 -0400

    Finish heap sort

--- sort/heap_sort.py
+++ sort/heap_sort.py
@@ -1,7 +1,7 @@
 class Heap:
     def __init__(self, n):
         self.size = 0
-        self.data = [None for i in range(10)]
+        self.data = [None for i in range(n)]
 
     def insert(self, item):
         self.data[self.size] = item
@@ -16,22 +16,22 @@ class Heap:
         return (self.size == 0)
 
     def pop(self):
-        if self.is_empty:
+        if self.is_empty():
             raise Exception("Heap is empty.")
 
         result = self.data[0]
 
-        self.data[0] = self.data[size - 1]
-        self.data[size - 1] = None
+        self.data[0] = self.data[self.size - 1]
+        self.data[self.size - 1] = None
         self.size -= 1
         i = 0
-        moreToSwitch = true
-        while (moreToSwitch):
+        more_to_switch = True
+        while (more_to_switch):
             smallest_ind = i
             smallest_val = self.data[i]
 
-            left_ind = i * 2
-            right_ind = i * 2 + 1
+            left_ind = i * 2 + 1
+            right_ind = i * 2 + 2
 
             if left_ind < self.size:
                 left_child = self.data[left_ind]
@@ -46,9 +46,25 @@ class Heap:
                     smallest_val - right_child
 
             if smallest_val == self.data[i]:
-                moreToSwitch = false
+                more_to_switch = False
             else:
                 self.data[i], self.data[smallest_ind] = self.data[smallest_ind], self.data[i]
                 i = smallest_ind
 
         return result
+
+def use_heap_sort():
+    arr = [7, 5, 17, 9, 31, 22]
+    print("unsorted array: ", arr)
+
+    heap = Heap(len(arr))
+    for ele in arr:
+        heap.insert(ele)
+
+    sorted_arr = []
+    while not heap.is_empty():
+        sorted_arr.append(heap.pop())
+    print("sorted array: ", sorted_arr)
+
+if __name__ == "__main__":
+    use_heap_sort()
